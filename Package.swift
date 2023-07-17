a076643c61578c32531714c6b72789cdf27faa8c
oscar <oscar@flashphoner.com>
2023-07-17 12:16:46 +0700

    Use FPWCSApi2 as standalone binary package with no dependencies #WCS-3862

--- Package.swift
+++ Package.swift
@@ -9,25 +9,14 @@ let package = Package(
     products: [
         .library(
             name: "FPWCSApi2",
-            type: .dynamic,
-            targets: ["FPWCSApi2Wrapper"]),
+            targets: ["FPWCSApi2"]),
     ],
-    dependencies: [
-        .package(name: "SocketRocket", url: "https://github.com/flashphoner/SocketRocketSPM.git", from: "1.0.0")
-    ],
+    dependencies: [],
     targets: [
         .binaryTarget(
             name: "FPWCSApi2",
             url: "https://flashphoner.com/downloads/builds/flashphoner_client/wcs-ios-sdk/pods/FPWCSApi2-2.6.111.zip",
             checksum: "c8462a674027130d7f3a59bcb74e816257a74b8b6a49349a76d8e4979f17de3c"
         ),
-        .target(
-            name: "FPWCSApi2Wrapper",
-            dependencies: [
-               .target(name: "FPWCSApi2Framework"),
-               .product(name: "SocketRocket",  package: "SocketRocket")
-            ],
-            path: "FPWCSApi2Wrapper"
-        )
     ]
 )
